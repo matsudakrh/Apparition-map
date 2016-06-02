fec6e16bc022b4d087efa08c2875e88d4d7a9588
matsuda <matsuda.ddcm@gmail.com>
2016-06-02 13:28:08 +0900

    Angularを適応（$scope.$apply()で再描画命令、$watch・$digestについて調べる必要有り?）

--- source/js/index.js
+++ source/js/index.js
@@ -1,18 +1,23 @@
-;( function ($) {
+
+var app = angular.module( 'MainApp', []);
+
+app.controller( 'MainController', function ( $scope ) {
+
+    var self = this;
+    console.log(self);
 
 
-    var jqMiniOpt = {
-    };
+    //self.searchList = ['shidhoi'];
 
-    $('#jqMini').jqMini(jqMiniOpt);
-
-
-    var $nameForm = $('#nameForm');
-    var $placeForm = $('#placeForm');
+    var $nameForm = document.getElementById('nameForm');
+    var $placeForm = document.getElementById('placeForm');
     var canvas = document.getElementById('mapResult');
-    
-    var $name = $('#name');
-    var $place = $('#place');
+
+    var $name = document.getElementById('name');
+    var $place = document.getElementById('place');
+
+
+    var virtualLink = document.createElement('a');
 
     function viewMap( searchText ) {
 
@@ -39,36 +44,43 @@
 
         var textRequest = {
             location: latlng,
-            radius: 400,
+            radius: 2000,
             query: searchText
         };
 
         var service = new google.maps.places.PlacesService(map);
-        service.textSearch(textRequest, function ( result ) {
+        service.textSearch(textRequest, function ( data ) {
 
-            console.log(result);
-            for ( var i = 0; i < result.length; i++ ) {
-                var place = result[i];
+            self.searchList = data;
+            console.log(data);
+            for ( var i = 0; i < data.length; i++ ) {
+                //self.searchList.push(data[i].name);
                 //createMarker(result[i]);
             }
-
+            //console.log(Array.isArray(data));
+            //self.searchList = data;
+            // console.log(self);
+            // console.log(self.searchList);
+            // console.log(self.searchList[0]);
+            // console.log(self.searchList[0].name);
+            $scope.$apply();
 
         });
-
 
     }
 
 
-    $nameForm.on( 'submit', function (e) {
+    $nameForm.addEventListener( 'submit', function (e) {
         e.preventDefault();
     });
 
-    $placeForm.on( 'submit', function (e) {
+    $placeForm.addEventListener( 'submit', function (e) {
         e.preventDefault();
 
-        var value = $place.val();
+        var value = $place.value;
         console.log(value);
-        console.log(e);
+        virtualLink.href = '#resultList';
+        virtualLink.click();
         viewMap(value);
 
     });
@@ -76,7 +88,7 @@
 
     /* ============================================= */
     /*
-          map.js
+     map.js
      */
     /* ============================================= */
 
@@ -84,43 +96,63 @@
     var canvas = document.getElementById('mapArea');
 
 
-    function getSuccessGeo( position ) {
-        canvas.classList.remove('loading');
+    function currentPosition() {
+        function getSuccessGeo( position ) {
+            canvas.classList.remove('loading');
 
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
 
 
-        var latlng = new google.maps.LatLng( lat , lng );
+            var latlng = new google.maps.LatLng( lat , lng );
 
-        var mapOptions = {
-            zoom: 15 ,				// ズーム値
-            center: latlng		// 中心座標 [latlng]
-        };
+            var mapOptions = {
+                zoom: 15 ,				// ズーム値
+                center: latlng		// 中心座標 [latlng]
+            };
 
-        var map = new google.maps.Map( canvas , mapOptions ) ;
+            var map = new google.maps.Map( canvas , mapOptions ) ;
+
+        }
+
+
+        if( navigator.geolocation ) {
+
+            navigator.geolocation.getCurrentPosition( function ( position ) {
+
+                getSuccessGeo(position);
+
+            } , function () {
+
+            } , {
+                enableHighAccuracy: true, // 精度を出来る限り高める
+                timeout: 100000, // タイムアウト設定
+                maximumAge: 100000 // ミリ秒間位置情報のキャッシュを保持
+            } ) ;
+        }
+
 
     }
+    currentPosition();
 
 
-    if( navigator.geolocation ) {
+    var $mapBtn = document.getElementById('mapBtn');
 
-        navigator.geolocation.getCurrentPosition( function ( position ) {
-
-            getSuccessGeo(position);
-
-        } , function () {
-
-        } , {
-            enableHighAccuracy: true, // 精度を出来る限り高める
-            timeout: 100000, // タイムアウト設定
-            maximumAge: 100000 // ミリ秒間位置情報のキャッシュを保持
-        } ) ;
-    }
+    $mapBtn.addEventListener( 'click', function () {
+        currentPosition();
+    });
 
 
 
 
+});
+
+;( function () {
 
 
-})(window.jQuery);
+
+
+
+
+
+})();
